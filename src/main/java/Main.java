fea8d57b83123c7230f4cccb2ef8129f7c803e44
Elis <Byhovtsov@gmail.com>
2021-06-24 17:21:45 +0300

    Task 5: add property

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,6 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("src/main/resources/config,properties");
-        Properties properties = new Properties();
-
-        try {
-            properties.load(new FileReader(file));
-            int month = Integer.parseInt(properties.getProperty("month"));
-            System.out.println(month);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
 
     }
 }

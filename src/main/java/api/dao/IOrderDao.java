813e8ec8e3187ac0da08f29e664ee93d03aee485
Elis <Byhovtsov@gmail.com>
2021-06-25 17:23:21 +0300

    Task 5: correction comments

--- src/main/java/api/dao/IOrderDao.java
+++ src/main/java/api/dao/IOrderDao.java
@@ -4,7 +4,7 @@ import model.Order;
 
 import java.util.List;
 
-public interface IOrderDao{
+public interface IOrderDao {
 
     void addOrder(Order order);
     void deleteOrder(Order order);
--- src/main/java/api/dao/IRequestDao.java
+++ src/main/java/api/dao/IRequestDao.java
@@ -5,7 +5,7 @@ import model.Request;
 
 import java.util.List;
 
-public interface IRequestDao{
+public interface IRequestDao {
 
     boolean add(Request request);
 

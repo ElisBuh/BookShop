b4f65c30a8d8cc7fa9f5068afcf89a2b3df51868
Elis <Byhovtsov@gmail.com>
2021-07-07 00:06:14 +0300

    Task 6: Ready

--- src/main/java/com/senla/ui/Starter.java
+++ src/main/java/com/senla/ui/Starter.java
@@ -1,14 +1,14 @@
 package com.senla.ui;
 
 import com.senla.ui.menu.MenuController;
-import com.senla.util.di.Supplication;
+import com.senla.util.di.Application;
 import com.senla.util.di.ApplicationContext;
 
 
 public class Starter {
 
     public static void main(String[] args) {
-        ApplicationContext context = Supplication.run("com.senla");
+        ApplicationContext context = Application.run("com.senla");
         MenuController menuController = context.getObject(MenuController.class);
         menuController.run();
 
--- src/main/java/com/senla/util/di/Application.java
+++ src/main/java/com/senla/util/di/Application.java
@@ -1,6 +1,6 @@
 package com.senla.util.di;
 
-public class Supplication {
+public class Application {
     public static ApplicationContext run(String packageToScan){
         JavaConfig config = new JavaConfig(packageToScan);
         ApplicationContext context = new ApplicationContext(config);

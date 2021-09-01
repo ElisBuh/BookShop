6ea700be241ee46b2104b8314e9d6b5d89a38b13
Elis <Byhovtsov@gmail.com>
2021-09-01 11:04:16 +0300

    Ref: add bean Spring/hibernate

--- src/main/java/com/senla/dao/StorageDao.java
+++ src/main/java/com/senla/dao/StorageDao.java
@@ -11,7 +11,7 @@ public class StorageDao extends AbstractDao<Storage> implements IStorageDao {
     @Override
     public Storage findStorageOnBook(Book book){
         log.info("Get_{}_By_: {}", Storage.class, book);
-        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+        Session session = sessionFactory.openSession();
         return (Storage) session.getNamedQuery("findStorageOnBook")
                 .setParameter("id",book.getId())
                 .getSingleResult();

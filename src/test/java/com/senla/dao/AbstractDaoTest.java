4e3458ca9cd9df5ede0c5bc5ab3c1c667eb7fba1
Elis <Byhovtsov@gmail.com>
2021-09-10 09:34:26 +0300

    JUnit Test

---
+++ src/test/java/com/senla/dao/AbstractDaoTest.java
@@ -0,0 +1,35 @@
+package com.senla.dao;
+
+import com.senla.api.dao.GenericDao;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class AbstractDaoTest {
+
+    GenericDao genericDaoMock = Mockito.mock(GenericDao.class);
+
+    @Test
+    void save() {
+
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void testGetAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+}
